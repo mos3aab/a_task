3f927989abcdf8a5d675be01bd79b0bfcf6ba20f
Mosabelwany <myusef595@gmail.com>
2022-07-25 18:42:04 +0200

    add Models and Controller

---
+++ DB_Dump.sql
@@ -0,0 +1,6 @@
+
+-- Create database named algoriza_task to connect to your app 
+CREATE DATABASE algoriza_task;
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// Route::get('/login',function()})->name('login');;
 Route::get('/', function () {
+    
     return view('welcome');
 });
